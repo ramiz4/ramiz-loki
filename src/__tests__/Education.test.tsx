d7bfc4d2d33756dba7c864dfcd369b95dd7b729a
Ramiz Loki <ramiz.loki@basenet.ch>
2025-05-16 14:06:24 +0200

    Import act from testing-library/react in Education tests

--- src/__tests__/Education.test.tsx
+++ src/__tests__/Education.test.tsx
@@ -1,5 +1,4 @@
-import { render, screen } from '@testing-library/react';
-import { act } from 'react';
+import { render, screen, act } from '@testing-library/react';
 import { Education } from '../components/Education';
 
 describe('Education', () => {
